a23973ebdbe274202386e7c62dbb30f7d12f46c9
Cory Weller <cory.a.weller@gmail.com>
2024-10-16 08:22:28 -0400

    simplify script

--- scripts/get-rsids.sh
+++ scripts/get-rsids.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-wget -O data/00-common_all.vcf.gz https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/00-common_all.vcf.gz
+vcf='data/00-common_all.vcf.gz'
+url='https://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/00-common_all.vcf.gz'
+rsids='data/rsids.txt.gz'
 
-zgrep -v -F '#' data/00-common_all.vcf.gz | awk '{print $1,$2,$3,$4,$5}' | gzip -c > rsids.txt.gz
+if [[ ! -f $vcf ]]; then
+    wget -O $vcf $url
+fi
+
+if [[ ! -f $rsids ]]; then
+    zgrep -v -F '#' $vcf | awk '{print $1,$2,$3,$4,$5}' | gzip -c > $rsids
+fi
